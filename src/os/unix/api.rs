7840ef1515474b66f2195ffb861a3382e5943e37
Evan Huff <ehuff007@gmail.com>
2024-06-24 14:07:49 -0400

    feat: implement basic process module

---
+++ src/os/unix/api.rs
@@ -0,0 +1,46 @@
+#[allow(non_camel_case_types, clippy::upper_case_acronyms)]
+mod types {
+	pub type pid_t = i32;
+	pub type c_int = i32;
+	pub type c_char = i8;
+	pub type size_t = usize;
+}
+
+#[allow(non_snake_case, unused, clippy::upper_case_acronyms)]
+mod constants {
+	use super::types::*;
+	pub const SIGTERM: c_int = 15;
+}
+
+#[allow(non_camel_case_types, clippy::upper_case_acronyms)]
+mod structs {
+	use super::{constants::*, types::*};
+
+	#[repr(C)]
+	pub struct utsname {
+		pub sysname: [c_char; 256],
+		pub nodename: [c_char; 256],
+		pub release: [c_char; 256],
+		pub version: [c_char; 256],
+		pub machine: [c_char; 256],
+	}
+}
+
+mod prototypes {
+	use super::{structs::*, types::*};
+	extern "C" {
+		pub fn kill(pid: pid_t, sig: c_int) -> c_int;
+		pub fn exit(status: c_int) -> !;
+		pub fn gethostname(name: *mut c_char, len: size_t) -> c_int;
+		pub fn uname(utsname: *mut utsname) -> c_int;
+		#[cfg(target_os = "macos")]
+		pub fn proc_pidinfo(pid: pid_t, flavor: u32, arg: u32, buffer: *mut c_void, buffersize: size_t) -> c_int;
+		#[cfg(target_os = "macos")]
+		pub fn proc_listallpids(buffer: *mut c_void, buffersize: size_t) -> c_int;
+	}
+}
+
+pub use constants::*;
+pub use prototypes::*;
+pub use structs::*;
+pub use types::*;
---
+++ src/os/unix/process.rs
@@ -0,0 +1,44 @@
+use std::process::Stdio;
+
+use tracing::{debug, error};
+
+use super::{
+	api::{exit, kill},
+	Unix,
+};
+use crate::{os::traits::process::ProcessModule, RuscordError};
+
+impl ProcessModule for Unix {
+	fn spawn(&self, name: &str, args: Option<String>) -> Result<(), RuscordError> {
+		let mut command = &mut std::process::Command::new(name);
+		command
+			.stderr(Stdio::piped())
+			.stdout(Stdio::piped())
+			.stdin(Stdio::piped());
+
+		if let Some(args) = args {
+			let split_args = args.split(" ").collect::<Vec<_>>();
+			debug!("args: {:?}", split_args);
+			command = command.args(split_args);
+		}
+		command.spawn()?;
+		Ok(())
+	}
+
+	fn kill_other(&self, pid: u32, exit_code: Option<u32>) -> Result<(), crate::RuscordError> {
+		let exit_code = exit_code.unwrap_or(15);
+		unsafe {
+			let result = kill(pid as i32, exit_code as i32);
+			if result != 0 {
+				error!("failed to kill process {}", pid);
+				return Err(std::io::Error::last_os_error().into());
+			}
+		}
+		Ok(())
+	}
+
+	fn kill_self(&self, exit_code: Option<u32>) -> ! {
+		let exit_code = exit_code.unwrap_or(15);
+		unsafe { exit(exit_code as i32) }
+	}
+}
